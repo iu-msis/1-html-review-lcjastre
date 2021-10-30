9d11de0d6aba5c8fa41660224db7403e3d6ac785
lcjastre <84337941+lcjastre@users.noreply.github.com>
2021-10-29 21:58:56 -0400

    changes made

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -49,12 +49,79 @@ const Offer = {
       });
      
     },
-  
+    postBook(evt) {
+      if (this.selectedBook === null) {
+          this.postNewBook(evt);
+      } else {
+          this.postEditBook(evt);
+      }
+    },
+    selectBook(s) {
+      this.selectedBook = s;
+      // this.offers = [];
+      this.offerForm= Object.assign({},this.selectedBook);
+      // this.fetchOfferData(this.selectedBooks);
   },
+    postEditBook(evt) {
+      this.offerForm.ID = this.selectedBook.ID;
+      // this.offerForm.id = this.selectedOffer.id;       
+      
+      console.log("Updating!", this.offerForm);
+
+      fetch('api/books/update.php', {
+          method:'POST',
+          body: JSON.stringify(this.offerForm),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8"
+          }
+        })
+        .then( response => response.json() )
+        .then( json => {
+          console.log("Returned from post:", json);
+          // TODO: test a result was returned!
+          this.books = json;
+          
+          this.resetOfferForm();
+        });
+    },
+    postDeleteBook(o) {
+      if (!confirm("Are you sure you want to delete the offer from "+o.Title+"?")) {
+          return;
+      }
+      
+      fetch('api/books/delete.php', {
+          method:'POST',
+          body: JSON.stringify(o),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8"
+          }
+        })
+        .then( response => response.json() )
+        .then( json => {
+          console.log("Returned from post:", json);
+          // TODO: test a result was returned!
+          this.books = json;
+          
+          this.resetOfferForm();
+        });
+    },
+    selectOffer(o) {
+      this.selectedOffer = o;
+      this.offerForm = Object.assign({}, this.selectedOffer);
+    },
+    resetOfferForm() {
+      this.selectedOffer = null;
+      this.offerForm = {};
+    }
+    
+},
+  
+  
   created() {
     this.fetchBookData();
   }
 }
+
   
   
 
